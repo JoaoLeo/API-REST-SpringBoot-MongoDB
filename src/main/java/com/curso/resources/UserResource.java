1a961a521e13674c53a71642d7692957c2a7e8cc
João Leonardo <joaoleonardo9921@gmail.com>
2023-06-26 11:56:04 -0300

    add entidade e resource de user

--- src/main/java/com/curso/resources/UserResource.java
+++ src/main/java/com/curso/resources/UserResource.java
@@ -1,2 +1,24 @@
-package com.curso.resources;public class UserResource {
+package com.curso.resources;
+
+import com.curso.domain.User;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResource {
+    @GetMapping
+    public ResponseEntity<List<User>> getAll(){
+        List<User> list = new ArrayList<>();
+        User teste1 = new User("1001", "teste1", "teste1@gmail.com");
+        User teste2 = new User("1002", "teste2", "teste2@gmail.com");
+        list.addAll(Arrays.asList(teste1, teste2));
+        return ResponseEntity.ok().body(list);
+    }
 }
