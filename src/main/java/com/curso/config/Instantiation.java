f95e45c39173449c0fa4a9f0158922102854311b
João Leonardo <joaoleonardo9921@gmail.com>
2023-06-27 09:23:19 -0300

    add config de instanciação do banco

---
+++ src/main/java/com/curso/config/Instantiation.java
@@ -0,0 +1,24 @@
+package com.curso.config;
+
+import com.curso.domain.User;
+import com.curso.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.Arrays;
+@Configuration
+public class Instantiation implements CommandLineRunner {
+
+    @Autowired
+    private UserRepository repo;
+
+    @Override
+    public void run(String... args) throws Exception {
+        repo.deleteAll();
+        User maria = new User(null, "Maria Brown", "maria@gmail.com");
+        User alex = new User(null, "Alex Green", "alex@gmail.com");
+        User bob = new User(null, "Bob Grey", "bob@gmail.com");
+        repo.saveAll(Arrays.asList(maria,alex,bob));
+    }
+}
