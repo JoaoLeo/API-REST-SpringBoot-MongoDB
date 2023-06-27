e1ea02d09958475cd40192da23e5b1356fc167a1
João Leonardo <joaoleonardo9921@gmail.com>
2023-06-27 11:47:52 -0300

    add post de user

--- src/main/java/com/curso/services/UserService.java
+++ src/main/java/com/curso/services/UserService.java
@@ -1,6 +1,7 @@
 package com.curso.services;
 
 import com.curso.domain.User;
+import com.curso.dto.UserDTO;
 import com.curso.repository.UserRepository;
 import com.curso.services.exception.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -20,5 +21,12 @@ public class UserService {
     public User findById(String id){
         Optional<User> user = repo.findById(id);
         return user.orElseThrow(() -> new ObjectNotFoundException("Objeto não encotrado na base de dados"));
+    }
+    public User insert(User user){
+        return repo.insert(user);
+    }
+    public User fromDTO(UserDTO dto){
+        User user = new User(dto.getId(), dto.getName(), dto.getEmail());
+        return user;
     }
 }
