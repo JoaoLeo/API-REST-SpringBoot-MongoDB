8e780675dcacb1f5343fb943fe38c636ff5e6d3b
João Leonardo <joaoleonardo9921@gmail.com>
2023-06-27 20:35:35 -0300

    add consultas mais complexas

--- src/main/java/com/curso/services/PostService.java
+++ src/main/java/com/curso/services/PostService.java
@@ -6,11 +6,13 @@ import com.curso.services.exception.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.Date;
 import java.util.List;
 import java.util.Optional;
 
 @Service
 public class PostService {
+
     @Autowired
     private PostRepository repo;
     public Post findById(String id){
@@ -19,5 +21,9 @@ public class PostService {
     }
     public List<Post> findByTitle(String text){
         return repo.findByTitle(text);
+    }
+    public List<Post> fullSearch(String text, Date minDate, Date maxDate){
+        maxDate =  new Date(maxDate.getTime() + 24 * 60 * 60 * 1000);
+        return repo.fullSearch(text,minDate,maxDate);
     }
 }
