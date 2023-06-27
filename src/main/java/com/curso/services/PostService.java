4a498461291339557b6aa1cd14081c5d74296620
João Leonardo <joaoleonardo9921@gmail.com>
2023-06-27 19:26:51 -0300

    add find by id em post

---
+++ src/main/java/com/curso/services/PostService.java
@@ -0,0 +1,19 @@
+package com.curso.services;
+
+import com.curso.domain.Post;
+import com.curso.repository.PostRepository;
+import com.curso.services.exception.ObjectNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import java.util.Optional;
+
+@Service
+public class PostService {
+    @Autowired
+    private PostRepository repo;
+    public Post findById(String id){
+        Optional<Post> Post = repo.findById(id);
+        return Post.orElseThrow(() -> new ObjectNotFoundException("Objeto não encotrado na base de dados"));
+    }
+
+}
