37f59172ed7640e157ccf32f2d64631122a29852
João Leonardo <joaoleonardo9921@gmail.com>
2023-06-27 19:59:55 -0300

    add consulta simples com query method

--- src/main/java/com/curso/resources/PostResource.java
+++ src/main/java/com/curso/resources/PostResource.java
@@ -1,10 +1,13 @@
 package com.curso.resources;
 
 import com.curso.domain.Post;
+import com.curso.resources.util.URL;
 import com.curso.services.PostService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping(value = "/posts")
@@ -15,6 +18,12 @@ public class PostResource {
     @GetMapping("/{id}")
     public ResponseEntity<Post> findById(@PathVariable String id){
         Post obj = service.findById(id);
+        return ResponseEntity.ok().body(obj);
+    }
+    @GetMapping("/titlesearch")
+    public ResponseEntity<List<Post>> findByTitle(@RequestParam(value = "text", defaultValue = "") String text){
+        text = URL.decodeParam(text);
+        List<Post> obj = service.findByTitle(text);
         return ResponseEntity.ok().body(obj);
     }
  }
--- src/main/java/com/curso/services/PostService.java
+++ src/main/java/com/curso/services/PostService.java
@@ -5,6 +5,8 @@ import com.curso.repository.PostRepository;
 import com.curso.services.exception.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -15,5 +17,7 @@ public class PostService {
         Optional<Post> Post = repo.findById(id);
         return Post.orElseThrow(() -> new ObjectNotFoundException("Objeto não encotrado na base de dados"));
     }
-
+    public List<Post> findByTitle(String text){
+        return repo.findByTitleContainingIgnoreCase(text);
+    }
 }
